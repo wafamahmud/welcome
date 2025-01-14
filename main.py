e0db1fd7f1ad1912f6c7fe9debfef2aed204d9d7
wafamahmud <wafamahmud8212@gmail.com>
2025-01-14 23:57:49 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+#print a sentence
+print("Welcome to the world of coding!")
+
+#print a number
+print(4)
+
+#next line after print
+print("Hello, Python!! \n")
+
+#Pass multiple arguments for printing
+print("Hello World", 5)
+
+#Solving simple problems
+print("3 + 5 is:", 3+5)
